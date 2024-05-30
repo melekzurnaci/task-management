ac0aae598af50f500559c4d44d2a8b28a92bddde
melek <mlkzrnc@gmail.com>
2024-05-30 18:55:29 +0300

    setting up authModule, User Entity and UserRepo

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User } from './user.entity';
+import { UserRepository } from './user.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [AuthService, UserRepository],
+  controllers: [AuthController],
+})
+export class AuthModule {}
